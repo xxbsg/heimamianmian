4dbbd5414528ec98ef9a9d0dab667e69dbc70dbd
xxg <qq1102746402@163.com>
2019-12-31 16:55:44 +0800

    实现删除功能

--- src/module-hmmm/components/dyh_articles.vue
+++ src/module-hmmm/components/dyh_articles.vue
@@ -19,26 +19,55 @@
         </template>
       </el-table-column>
     </el-table>
+    <el-row type='flex' justify='end'>
+         <el-pagination
+    layout="prev, pager, next"
+    :current-page='page.currentpage'
+    :page-sizes='page.pagesizes'
+    :total="page.total"
+    @current-change='changepage'>
+  </el-pagination>
+    </el-row>
+    <el-dialog :visible.sync="dialogVisible" title="收货地址"></el-dialog>
   </el-card>
 </template>
 
 <script>
 import {
   list as articl_list,
-  state as article_state
+  state as article_state,
+  remove as article_remove
 } from "../../api/hmmm/articles";
 export default {
   data() {
     return {
-      tableData: []
+        dialogVisible:false,//弹窗显示
+      tableData: [],
+      page:{
+          currentpage:1,
+          total:0,
+          pagesize:10,
+      },
+      keyword:null
     };
   },
   methods: {
+      //切换页码
+      changepage(newval){
+          this.page.currentpage=newval
+          this.getarticle()
+      },
     //请求列表
     getarticle() {
-      articl_list().then(res => {
+        let data={
+            page:this.page.currentpage,
+            pagesize:this.page.pagesize,
+            keyword:this.keyword
+        }
+      articl_list(data).then(res => {
         console.log("定义一个", res.data);
         this.tableData = res.data.items;
+        this.page.total=res.data.counts;
       });
     },
     //格式话状态
@@ -64,11 +93,25 @@ export default {
         });
       });
     },
+    //编辑
     handleEdit(index, row) {
+        this.dialogVisible=!this.dialogVisible
       console.log(index, row);
     },
+    //删除
     handleDelete(index, row) {
       console.log(index, row);
+      this.$confirm(`此操作将删除该文章, 是否继续?`, "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      }).then(() => {
+        let data = { id: row.id };
+        article_remove(data).then(res => {
+          console.log(res);
+          this.getarticle();
+        });
+      });
     }
   },
   filters: {
